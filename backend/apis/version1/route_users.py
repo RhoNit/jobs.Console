7c462bb1d7b0a1d11abd5b441b9b23ec4dc5fd8b
Ranit Biswas <60309029+RhoNit@users.noreply.github.com>
2022-12-01 12:32:40 +0530

    [update] formatted code

--- backend/apis/version1/route_users.py
+++ backend/apis/version1/route_users.py
@@ -1,15 +1,18 @@
+
+from fastapi import APIRouter, Depends
+
+import sqlalchemy.orm as _orm
+
 from db.repository.users import create_new_user
 from db.session import get_db
-from fastapi import APIRouter
-from fastapi import Depends
-from schemas.users import ShowUser
-from schemas.users import UserCreate
-from sqlalchemy.orm import Session
+
+from schemas.users import UserCreate, ShowUser
+
 
 router = APIRouter()
 
 
 @router.post("/", response_model=ShowUser)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    user = create_new_user(user=user, db=db)
-    return user
+def create_user(user: UserCreate, db: _orm.Session = Depends(get_db)):
+    user_obj = create_new_user(user=user, db=db)
+    return user_obj
