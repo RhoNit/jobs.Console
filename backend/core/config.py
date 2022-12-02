750ac94979b15eca3ad2e89ec68d278fab315697
Ranit Biswas <60309029+RhoNit@users.noreply.github.com>
2022-12-02 07:52:31 +0530

    [add] added function to return database uri

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-env_path = Path(".") / ".env"
+env_path = Path(__file__).parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 
@@ -15,15 +15,22 @@ class Settings:
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)  # default postgres port is 5432
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)            # default postgres port is 5432
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30  # in mins
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-    TEST_USER_EMAIL = "test@example.com"
+    TEST_USER_EMAIL = "test@gmail.com"
 
 
 settings = Settings()
+
+
+def get_db_uri() -> str:
+    if settings.USER_SQLITE_DB:
+        return "sqlite:///./datastore.db"
+    else:
+        return settings.DATABASE_URL          # for postgres db 
