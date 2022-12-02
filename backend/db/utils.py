b56447c22d85326f4067dac1e6310a9c044e2b22
Ranit Biswas <60309029+RhoNit@users.noreply.github.com>
2022-12-02 08:11:06 +0530

    [update] imported get_db_uri() function from config.py

--- backend/db/utils.py
+++ backend/db/utils.py
@@ -1,26 +1,30 @@
 import databases
-from db.session import SQLALCHEMY_DATABASE_URL
+from configurator.config import get_db_uri
+
+uri = get_db_uri()
 
 
 async def check_db_connected():
     try:
-        if not str(SQLALCHEMY_DATABASE_URL).__contains__("sqlite"):
-            database = databases.Database(SQLALCHEMY_DATABASE_URL)
+        if not str(uri).__contains__("sqlite"):
+            database = databases.Database(uri)
             if not database.is_connected:
                 await database.connect()
                 await database.execute("SELECT 1")
-        print("Database is connected (^_^)")
+        print("Database is connected (^_^) : UP")
+        
     except Exception as e:
-        print("Looks like db is missing or is there is some problem in connection,see below traceback")
+        print("Seems like DB is missing or there is a problem in connection, see below traceback")
         raise e
 
 
 async def check_db_disconnected():
     try:
-        if not str(SQLALCHEMY_DATABASE_URL).__contains__("sqlite"):
-            database = databases.Database(SQLALCHEMY_DATABASE_URL)
+        if not str(uri).__contains__("sqlite"):
+            database = databases.Database(uri)
             if database.is_connected:
                 await database.disconnect()
-        print("Database is Disconnected (-_-) zZZ")
+        print("Database is disconnected (-_-) : DOWN")
+        
     except Exception as e:
         raise e
